9b8ceccecd7a3e45b9f89f018f0a5f6adb52e3eb
chrisrudzki <chrisrudzki7@gmail.com>
2025-06-16 14:35:42 -0700

    changed text again

--- src/components/AboutContent.jsx
+++ src/components/AboutContent.jsx
@@ -12,7 +12,7 @@ export default function AboutContent({ onDisplay }) {
                 //style={{ aspect-ratio: "4/3" }, { width: 300px; }}
               />
     <div class="text-cont">
-      <p>Hello, <br /> <br /> I'm Christopher Rudzki, a software developer currently based in Victoria, Canada. I am a Computer Science and Visual Arts undergrad at the University of Victoria. <br /><br />   With expirence in fullstack development, algorithm, game development, graphics programming design and art I am open to new horizons and any opportunities. <br /> <br /> Please do not hesitate to reach out. <br /> <br /> <br /> <br />  chrisrudzki7@gmail.com</p>
+      <p>Hello, <br /> <br /> I'm Christopher Rudzki, a software developer currently based in Victoria, Canada. I am a Computer Science and Visual Arts undergrad at the University of Victoria. <br /><br />   With expirence in fullstack development, algorithm design, game development, graphics programming design and art I am open to new horizons and any opportunities. <br /> <br /> Please do not hesitate to reach out. <br /> <br /> <br /> <br />  chrisrudzki7@gmail.com</p>
       </div>
    </div>
     </>
