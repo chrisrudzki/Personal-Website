549ce118ebfb6b176d0bcf36e7076f8d56465902
chrisrudzki <chrisrudzki7@gmail.com>
2025-06-16 13:11:25 -0700

    changed text and prof image

--- src/components/AboutContent.jsx
+++ src/components/AboutContent.jsx
@@ -8,10 +8,11 @@ export default function AboutContent({ onDisplay }) {
                 key="pers"
                 src={`/Photos/PersonalPhoto.png`}
                 alt={`personal photo`}
-                style={{ height: "5px" }, { width: "17%" }}
+                style={{ aspectRatio: "4 / 3", width: "300px" }}
+                //style={{ aspect-ratio: "4/3" }, { width: 300px; }}
               />
     <div class="text-cont">
-      <p>Hello, <br /> <br /> I'm Christopher Rudzki. I am a software developer currently baised in Victoria, Canada. I am in my 3rd year Computer Science and Visual Arts undergrad at the University of Victoria. <br /><br />   I am interested in fullstack development, game development, graphics programming, design and art. <br /> <br /> In my free time I enjoy drawing and painting. <br /> <br /> Please do not hesitate to reach out. <br /> <br /> <br /> <br />  chrisrudzki7@gmail.com</p>
+      <p>Hello, <br /> <br /> I'm Christopher Rudzki, a software developer currently based in Victoria, Canada. I am a Computer Science and Visual Arts undergrad at the University of Victoria. <br /><br />   With expirence in fullstack development, algorithm, game development, design and art I am open to new horizons and any opportunities. <br /> <br /> In my free time I enjoy drawing and painting. <br /> <br /> Please do not hesitate to reach out. <br /> <br /> <br /> <br />  chrisrudzki7@gmail.com</p>
       </div>
    </div>
     </>
