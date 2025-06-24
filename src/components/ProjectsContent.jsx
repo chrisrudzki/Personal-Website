cd738c1e07107e514f4783520a4384b014e8fa5f
chrisrudzki <chrisrudzki7@gmail.com>
2025-06-24 14:55:54 -0700

    changed disc, icon

--- src/components/ProjectsContent.jsx
+++ src/components/ProjectsContent.jsx
@@ -1,6 +1,14 @@
 export default function ProjectsContent({ onDisplay }) {
   const disc_text = <p> Developed in Gadot using GD Script, Chicken Fun is an action shooter inspired by zombie horde zombie games. You play as a lonesome boy who has been suddenly attacked by ducks. How long can you last? </p>;
   
+  const disc_text_2 = <p> Enemy design
+
+Yellow ducks are designed with popular bird like flocking algorithm by Creig Ryenolds. Their position is determined by the centre of mass collision avoidance, and velocity of nearby birds along with direction towards the player. This enemy tracking allows for dynamic player navigation between clumps of enemies when many are closely chasing. Spawn locations are limited to 2 and rotate between 9 around the island to promote clumping. 
+
+A map with separate areas between barriers would increase dynamic navigation at cost to the game's simplicity.
+
+</p>
+
   const video1 = document.getElementById("slide-1")
   
   const chickenFunPhotos = [
@@ -12,6 +20,8 @@ export default function ProjectsContent({ onDisplay }) {
 
   return (
     <>
+
+
          <div class="content-container">
             <div class="proj-col">
             <h2>Chicken Fun</h2>
@@ -30,10 +40,16 @@ export default function ProjectsContent({ onDisplay }) {
             <h2>features</h2>
             
             <ul>
+                
                 <li>3 unique wepons</li>
                 <li>3 unique enemies</li>
                 <li>currenty system with weapeon upgrade tree</li>
                 <li>intellegent enemy flocking</li>
+
+                {disc_text_2}
+
+
+
             </ul>
           
         <div class="gallery-row">
