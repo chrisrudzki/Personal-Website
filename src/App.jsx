29754657b4d88a083fc134016c651ee7e3392a78
chrisrudzki <chrisrudzki7@gmail.com>
2025-06-24 14:59:39 -0700

    changed disc, icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 //import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import viteLogo from '/web_icon.svg'
 import './App.css'
 
 import ArtContent from './components/ArtContent.jsx'
