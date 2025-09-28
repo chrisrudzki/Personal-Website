a6b12bb70e6289b3af3daa73678f276fc52f5eaa
chrisrudzki <chrisrudzki7@gmail.com>
2025-09-28 15:04:52 -0700

    a

--- src/components/AboutContent.jsx
+++ src/components/AboutContent.jsx
@@ -36,7 +36,7 @@ export default function AboutContent({ onDisplay }) {
         
     <div className="content-container-pfp">
     <div class="text-cont">
-      <p>Hello, <br /> <br /> I'm Christopher Rudzki, software developer currently based in Canada studying Computer Science at the University of Victoria. <br /><br />   With expirence in fullstack development, algorithm design, game development, graphics programming, design and art I am open to new horizons and opportunities. <br /> <br /> Feel free to reach out. <br /> <br /> <br /> <br />  chrisrudzki7@gmail.com</p>
+      <p>Hello, <br /> <br /> I'm Christopher Rudzki, a software developer currently based in Canada studying Computer Science at the University of Victoria. <br /><br />   With expirence in fullstack development, algorithm design, game development, design and art I am open to new horizons and opportunities. <br /> <br /> Feel free to reach out. <br /> <br /> <br /> <br />  chrisrudzki7@gmail.com</p>
       </div>
     </div>
    </div>
