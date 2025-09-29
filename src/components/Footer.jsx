83cecbc5c5fb5e782a8013fda649a25fea40c5ba
chrisrudzki <chrisrudzki7@gmail.com>
2025-09-29 13:30:15 -0700

    a

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,14 +5,13 @@ export default function Footer(){
 
     return(
         <>
-        <div className="footer">
+        <div class="footer">
 
         <div className="footer-inner">
 
-
         <img 
                 //className="zoom-image"
-                key="pers"
+                key="linked"
                 src="/Photos/linkedin.png"
                 alt={`linkedin`}
                 style={{ width: "40px" }}
@@ -21,7 +20,7 @@ export default function Footer(){
 
          <img 
                 //className="zoom-image"
-                key="pers"
+                key="git"
                 src="/Photos/github.png"
                 alt={`github`}
                 style={{ width: "40px" }}
